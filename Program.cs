d9ddbfc560c23b587bf597dea4ffacabaffcba20
Kittichai <kittichai.kitt@mail.kmutt.ac.th>
2023-05-20 22:11:07 +0700

    Homework1

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rowNumber;
+        bool validInput = false;
+
+        do
+        {
+            Console.Write("Please enter the number of layers of the Pascal triangle: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out rowNumber) && rowNumber >= 0)
+            {
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pascal's triangle row number.");
+            }
+        } while (!validInput);
+
+        for (int i = 0; i <= rowNumber; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(CalculatePascalValue(i, j) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int CalculatePascalValue(int row, int column)
+    {
+        if (column == 0 || column == row)
+        {
+            return 1;
+        }
+        else
+        {
+            return CalculatePascalValue(row - 1, column - 1) + CalculatePascalValue(row - 1, column);
+        }
+    }
+}
